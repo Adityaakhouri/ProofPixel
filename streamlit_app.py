7eebfd30f87df2dd4ae156c35d4df1bef90d0ce2
Aditya Akhouri <97386637+Adityaakhouri@users.noreply.github.com>
2025-05-15 04:22:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,34 +26,25 @@ def predict(image, model):
 st.set_page_config(page_title="AI vs Real Classifier", layout="centered")
 
 # ------------ Navigation Menu ------------
-menu = st.columns([8, 1, 1])
+menu = st.columns([8, 2, 2])
 with menu[1]:
     if st.button("Home"):
         st.experimental_rerun()
 with menu[2]:
     if st.button("About Us"):
-        st.info("This AI model classifies whether an image is AI-generated or real using a CNN-based model. Built by Aditya Akhouri.")
+        st.info("This AI model classifies whether an image is AI-generated or real using a CNN-based model. Built by Aditya Akhouri and Tanusree Saha")
 
-st.markdown("---")
+st.markdown("-")
 
 # ------------ Image Display + Upload UI ------------
 st.title("AI vs Real Image Classifier")
 st.write("Upload an image to detect whether it is AI-generated or Real.")
 
-layout = st.columns(2)
+layout = st.columns(1)
 
-# Left Column: Placeholder or Uploaded Image
-with layout[0]:
-    st.subheader("Image Display")
-    if 'uploaded_image' not in st.session_state:
-        # Display placeholder box
-        placeholder = Image.new("RGB", (256, 256), color=(230, 230, 230))
-        st.image(placeholder, caption="No Image Uploaded", use_column_width=True)
-    else:
-        st.image(st.session_state['uploaded_image'], caption="Uploaded Image", use_column_width=True)
 
 # Right Column: Upload & Prediction
-with layout[1]:
+with layout[0]:
     uploaded_file = st.file_uploader("Upload your image here", type=["jpg", "jpeg", "png"])
     if uploaded_file:
         image = Image.open(uploaded_file).convert("RGB")
